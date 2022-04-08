42f784034f179d15b52c933713e49cc0d13721a8
Shraddha Bhardwaj <66181120+shraddha-bhardwaj@users.noreply.github.com>
2022-04-08 16:16:16 +0530

    Create find max and min element in minimum comparisons.cpp

---
+++ find max and min element in minimum comparisons.cpp
@@ -0,0 +1,46 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a[]={3,6,7,2,1,9};
+    int i=0;
+    int n=sizeof(a)/sizeof(a[0]);
+    int max=0;
+    int min=0;
+    if(n%2==0){
+        if(a[0]>a[1]){
+         max=a[0];
+         min=a[1];
+        }
+        else{
+        max=a[1];
+        min=a[0];
+        i=2;}
+    }
+    else{
+        max=a[0];
+        min=a[0];
+        i=1;
+    }
+    while(i<n-1){
+        if(a[i]>a[i+1]){
+            if(a[i]>max)
+                max=a[i];
+            if(a[i+1]<min)
+                min=a[i+1];
+        }
+        else{
+            if(a[i+1]>max){
+                max=a[i+1];
+            }
+            if(a[i]<min)
+                min=a[i];
+        }
+        i=i+2;
+    }
+    cout<<max<<endl<<min<<endl;
+
+	return 0;
+}
+
