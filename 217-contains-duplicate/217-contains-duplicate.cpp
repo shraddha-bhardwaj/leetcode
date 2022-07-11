d933b908a0f2a95964d79a553a0030360f309101
Shraddha Bhardwaj <66181120+shraddha-bhardwaj@users.noreply.github.com>
2022-07-11 17:45:11 +0530

    Time: 196 ms (14.26%), Space: 46.6 MB (79.00%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        /*for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]) return true;
+            }
+        }
+        return false;
+        time complexity - O(N^2)
+        */
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]) return true;
+        }
+        return false;
+    }
+};
