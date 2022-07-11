0da9a68539a8ca524ff59103f4aabc7aeda7a48c
Shraddha Bhardwaj <66181120+shraddha-bhardwaj@users.noreply.github.com>
2022-07-11 18:19:06 +0530

    Time: 97 ms (94.75%), Space: 53.6 MB (20.03%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -9,9 +9,22 @@ public:
         return false;
         time complexity - O(N^2)
         */
+        
+        /* 
+        TC- O(Nlog(N))
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]==nums[i+1]) return true;
+        }
+        return false;
+        */
+        
+    //TC- O(N) SC- O(N) - hashmap- unordered map 
+      
+    unordered_map<int,int>m;
+        for(auto a: nums)m[a]++;
+        for(auto a: m){
+            if(a.second>1) return true;
         }
         return false;
     }
