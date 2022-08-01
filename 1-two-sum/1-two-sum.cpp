224f9f5f8bf173efc0a666c39b8de1c5d162527d
Shraddha Bhardwaj <66181120+shraddha-bhardwaj@users.noreply.github.com>
2022-08-01 20:59:45 +0530

    Time: 14 ms (82.95%), Space: 11 MB (24.31%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -10,6 +10,8 @@ public:
             }
         }
         return{-1,-1};*/
+        
+    /* using map
        int n=nums.size();
        unordered_map<int,int>m;
         for(int i=0;i<n;i++){
@@ -19,6 +21,27 @@ public:
                 return {i,m[y]};
             }
             m[nums[i]]=i;
+        }
+        return {-1,-1};
+        */
+        vector<pair<int,int>> v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back({nums[i],i});
+        }
+        sort(v.begin(),v.end());
+        int l=0;
+        int h=nums.size()-1;
+        if(nums.size()==2){
+            return {0,1};
+        }
+        while(l<h){
+            if((v[l].first+v[h].first)==target)
+                return {v[l].second,v[h].second};
+            else if((v[l].first+v[h].first)>target)
+                h--;
+            else if((v[l].first+v[h].first)<target)
+                l++;
+            
         }
         return {-1,-1};
     }
